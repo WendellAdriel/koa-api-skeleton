b34cab376d559c87080669fb7d9264b50ebfa716
Wendell Adriel <wendelladriel.ti@gmail.com>
2017-03-08 16:11:23 +0000

    fixed error with babel in src and test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 process.env.NODE_ENV = 'development';
 
 require('babel-register')
+require('babel-polyfill')
 require('./src/server.js')
--- test/server.spec.js
+++ test/server.spec.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill'
 import server from '~/server'
 import supertest from 'supertest'
 
